67f722104149821af36b215e31105a5cb492530b
veronika333 <vera3@inbox.ru>
2020-09-23 09:51:14 +0300

    routing and pages added

---
+++ src/Routes.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Signup from './user/Signup';
+import Signin from './user/Signin';
+import Home from './core/Home';
+
+export default function Routes() {
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route path='/home' exact component={Home}></Route>
+                <Route path='/signin' exact component={Signin}></Route>
+                <Route path='/signup' exact component={Signup}></Route>
+            </Switch>
+        </BrowserRouter>
+    )
+}
