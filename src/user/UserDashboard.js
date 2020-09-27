25fadc94a673b4fc4e224f871ba6def3080812ba
veronika333 <vera3@inbox.ru>
2020-09-27 19:30:03 +0300

    links on user dashboard added

--- src/user/UserDashboard.js
+++ src/user/UserDashboard.js
@@ -1,25 +1,62 @@
 import React from 'react';
 import Layout from '../core/Layout';
 import { isAuthenticated } from '../auth/index';
+import { Link } from 'react-router-dom';
 
  const UserDashboard = () => {
 
  const {user: {_id, name, email, role}} = isAuthenticated()   //destructing isAuthenticated
-    return (
-        <Layout title="Dashboard" description="User dashboard" className="container">
-<div className="card mb-5">
-    <h3 className="card-header">User information</h3>
+   
+ const userLinks = () => {
+     return (
+        <div className="card">
+<h4 className="card-header">User Links</h4>
 <ul className="list-group">
-<li className="list-group-item">{name}</li>
-<li className="list-group-item">{email}</li>
-<li className="list-group-item">{role === 1 ? 'Admin' : 'Registered User'}</li>
+<li className="list-group-item">
+    <Link className="nav-link" to="/cart">My cart</Link>
+</li>
+<li className="list-group-item">
+    <Link className="nav-link" to="/profile/update">Update profile</Link>
+</li>
 </ul>
-</div>
-<div className="card mb-5">
+        </div>
+     )
+ }
+
+ const userInfo = ()=> {
+     return (
+         <div className="card mb-5">
+<h3 className="card-header">User information</h3>
+<ul className="list-group">
+     <li className="list-group-item">{name}</li>
+     <li className="list-group-item">{email}</li>
+     <li className="list-group-item">{role === 1 ? 'Admin' : 'Registered User'}</li>
+</ul>
+         </div>
+     )
+ }
+ 
+const purchaseHistory = () => {
+    return (
+        <div className="card mb-5">
 <h3 className="card-header">Purchase history</h3>
 <ul className="list-group">
 <li className="list-group-item">history</li>
 </ul>
+</div>
+    )
+}
+//in a row, create 12 colummn grid(bootstrap) with 3 and 9 columns
+ return (
+        <Layout title="Dashboard" description={`Hello ${name}!`} className="container-fluid">
+<div className="row">
+<div className="col-3"> 
+{userLinks()}
+</div>
+<div className="col-9">
+{userInfo()}
+{purchaseHistory()}
+</div>
 </div>
         </Layout>
     )
