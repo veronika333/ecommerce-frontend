ea54e11059591fffa3a120cbfbc913fde74fab03
veronika333 <vera3@inbox.ru>
2020-09-30 14:17:24 +0300

    css added

--- src/core/Card.js
+++ src/core/Card.js
@@ -12,7 +12,7 @@ const Card = ({ product }) => {
         <p>{product.description}</p>
     <p>{product.price}€</p>
     <Link to="/">
-        <button className="btn btn-outline-primary mt-2 mb-2">View Product</button>
+        <button className="btn btn-outline-primary mt-2 mb-2 mr-2">View Product</button>
     </Link>
     <button className="btn btn-outline-warning mt-2 mb-2">Add to Card</button>
     </div>
