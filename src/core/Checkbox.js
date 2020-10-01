531a8cb5e3388902546b65fd5caaec9c20390660
veronika333 <vera3@inbox.ru>
2020-10-01 18:52:35 +0300

    can console.lo categories id in checkbox

--- src/core/Checkbox.js
+++ src/core/Checkbox.js
@@ -2,9 +2,27 @@ import React, { useState, useEffect } from 'react';
 
 //props - categories from the Shop; look through categories and return all of them
 const Checkbox = ({categories}) => {
+ const [checked, setChecked] = useState([])
+
+ //gets the category and returns another function
+ //it will fire every time when there is onChange event in input
+ const handleToggle = category => () => {
+const currentCategoryId = checked.indexOf(category) //check if the category is already in the state; if it's not found in the state , it will return -1; otherwise returns the first index found
+const newCheckedCategoryId = [...checked] 
+//if now checked was not in checked state, then pushing
+//otherwise pull
+if(currentCategoryId === -1) {
+    newCheckedCategoryId.push(category)
+} else { //if it was already there( it was checked) . if it was checked, need to uncheck
+newCheckedCategoryId.splice(currentCategoryId, 1) //grab one item and take it off
+}
+console.log(newCheckedCategoryId)
+setChecked(newCheckedCategoryId)
+}
+    
 return categories.map((category, index) => (
     <li key={index} className="list-unstyled">
-       <input type="checkbox" className="form-check-input" /> 
+       <input onChange={handleToggle(category._id)} value={checked.indexOf(category._id === -1)} type="checkbox" className="form-check-input" /> 
 <lable className="form-check-label">{category.name}</lable>
     </li>
 ))
