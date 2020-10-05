57d2f4de25d706df387df2edbd749a98177e105e
veronika333 <vera3@inbox.ru>
2020-10-05 20:03:31 +0300

    one more typo removed

--- src/core/Shop.js
+++ src/core/Shop.js
@@ -8,16 +8,16 @@ import { prices } from './fixedPrices';
 
 
 const Shop = () => {
-const [myFilters, setMyFilters] = useState({
-    filters: { category: [], price: [] } //filters contain categories and price
-});
+// const [myFilters, setMyFilters] = useState({
+//     filters: { category: [], price: [] } //filters contain categories and price
+// });
     //creating state to hold categories
     const [categories, setCategories] = useState([]);
     const [error, setError] = useState(false);
-    const [limit, setLimit] = useState(6);
-    const [skip, setSkip] = useState(0);
-    const [size, setSize] = useState(0);
-    const [filteredResults, setFilteredResults] = useState(0);
+    // const [limit, setLimit] = useState(6);
+    // const [skip, setSkip] = useState(0);
+    // const [size, setSize] = useState(0);
+    // const [filteredResults, setFilteredResults] = useState(0);
 
 //load categories 
 const init = () => {
@@ -30,82 +30,90 @@ const init = () => {
     });
 };
 
-const loadFilteredResults = (newFilters) => {
-   // console.log(newFilters)
-   getFilteredProducts(skip, limit, newFilters).then(data => {
-       if(data.error){
-           setError(data.error)
-       } else {
-           //take everything in filteredResults and then pass the data
-setFilteredResults(data.data); //checked in console in network, everytning inside data
-setSize(data.size) //ow many products getting
-setSkip(0) //later can use it to load more
-}
-   })
-}
+// const loadFilteredResults = (newFilters) => {
+//    // console.log(newFilters)
+//    getFilteredProducts(skip, limit, newFilters).then(data => {
+//        if(data.error){
+//            setError(data.error)
+//        } else {
+//            //take everything in filteredResults and then pass the data
+// setFilteredResults(data.data); //checked in console in network, everytning inside data
+// setSize(data.size) //ow many products getting
+// setSkip(0) //later can use it to load more
+// }
+//    })
+// }
 //load more method
 
-const loadMore = () => {
-   let toSkip = skip + limit //skip whatever is in the state + limit
-    getFilteredProducts(toSkip, limit, myFilters.filters).then(data => {
-        if(data.error){
-            setError(data.error)
-        } else {
- setFilteredResults([...filteredResults, ...data.data]); //check whatever is in the state and add data
- setSize(data.size)    //setting size again  
- setSkip(toSkip)
- }
-    })
- }
+// const loadMore = () => {
+//    let toSkip = skip + limit //skip whatever is in the state + limit
+//     getFilteredProducts(toSkip, limit, myFilters.filters).then(data => {
+//         if(data.error){
+//             setError(data.error)
+//         } else {
+//  setFilteredResults([...filteredResults, ...data.data]); //check whatever is in the state and add data
+//  setSize(data.size)    //setting size again  
+//  setSkip(toSkip)
+//  }
+//     })
+//  }
 
-const loadMoreButton = () => {
-    return (
-        size > 0 && size >= limit && (
-            <button onClick={loadMore} className="btn btn-warning mb-5">Load more</button>
-        )
-    )
-}
-
-useEffect = (() => {
-    init(); //run these function when the component mounts
-    // loadFilteredResults(skip, limit, myFilters.filters) //passing the arguments
-    }, []);
+// const loadMoreButton = () => {
+//     return (
+//         size > 0 && size >= limit && (
+//             <button onClick={loadMore} className="btn btn-warning mb-5">Load more</button>
+//         )
+//     )
+// }
+useEffect(() => {
+init()
+}, [])
+// useEffect = (() => {
+//     init(); //run these function when the component mounts
+//     // loadFilteredResults(skip, limit, myFilters.filters) //passing the arguments
+//     }, []);
 //pass the methode below to the checkbox
 //filterBy - either by category or by price
-const handleFilters = (filters, filterBy) => {
-//console.log("SHOP", filters, filterBy)
-const newFilters = {...myFilters};
-newFilters.filters[filterBy] = filters;
 
-if(filterBy === "price"){
-    let priceValues = handlePrice(filters);
-    newFilters.filters[filterBy] = priceValues;
-}
-loadFilteredResults(myFilters.filters) //fetch filtered products
-setMyFilters(newFilters)
-}
+//need code below
+
+// const handleFilters = (filters, filterBy) => {
+// //console.log("SHOP", filters, filterBy)
+// const newFilters = {...myFilters};
+// newFilters.filters[filterBy] = filters;
+
+// if(filterBy === "price"){
+//     let priceValues = handlePrice(filters);
+//     newFilters.filters[filterBy] = priceValues;
+// }
+// loadFilteredResults(myFilters.filters) //fetch filtered products
+// setMyFilters(newFilters)
+// }
 
 //grabbing the value
-const handlePrice = value => {
-const data = prices;
-let array = []
+// const handlePrice = value => {
+// const data = prices;
+// let array = []
 
-//looping prices
-for(let key in data){ //looping through each
-    if(data[key]._id === parseInt(value)){ //The parseInt function converts its first argument to a string, parses that string, then returns an integer or NaN
-        //populate array 
-        array = data[key].array
-    }
-}
-return array; //return array after looping
-}
+// //looping prices
+// for(let key in data){ //looping through each
+//     if(data[key]._id === parseInt(value)){ //The parseInt function converts its first argument to a string, parses that string, then returns an integer or NaN
+//         //populate array 
+//         array = data[key].array
+//     }
+// }
+// return array; //return array after looping
+// }
 
 
 
     return (
-        <Layout title="Shop Page" descriptio="Shop and find makeups of your choice" className="container-fluid">
+        <Layout title="Shop Page" description="Shop and find makeups of your choice" className="container-fluid">
+            <div>
+                /* {JSON.stringify(categories)} */
+            </div>
 
-<div className="row">
+{/* <div className="row">
     <div className="col-4">
 
         <h4>Filter by categories</h4>
@@ -135,7 +143,7 @@ return array; //return array after looping
    <hr />
    {loadMoreButton()}
    </div>
-</div>
+</div> */}
 
         </Layout>
     )
