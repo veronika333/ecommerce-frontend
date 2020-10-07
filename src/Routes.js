e63a38c1a796f04887173607eee792759bccf31e
Olabisi <53159103+olabee87-stack@users.noreply.github.com>
2020-10-07 19:02:09 +0300

    Add Orer route to routes.js

--- src/Routes.js
+++ src/Routes.js
@@ -1,29 +1,47 @@
-import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Signup from './user/Signup';
-import Signin from './user/Signin';
-import Home from './core/Home';
-import PrivateRoute from './auth/PrivateRoute';
-import UserDashboard from './user/UserDashboard';
-import AdminRoute from './auth/AdminRoute';
-import AdminDashboard from './user/AdminDashboard';
-import AddCategory from './admin/AddCategory';
-import AddProduct from './admin/AddProduct';
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Signup from "./user/Signup";
+import Signin from "./user/Signin";
+import Home from "./core/Home";
+import PrivateRoute from "./auth/PrivateRoute";
+import UserDashboard from "./user/UserDashboard";
+import AdminRoute from "./auth/AdminRoute";
+import AdminDashboard from "./user/AdminDashboard";
+import AddCategory from "./admin/AddCategory";
+import AddProduct from "./admin/AddProduct";
 import Shop from "./core/Shop";
+import Orders from "./admin/Orders";
 
 export default function Routes() {
-    return (
-        <BrowserRouter>
-            <Switch>
-                <Route path='/' exact component={Home}></Route>
-                <Route path='/shop' exact component={Shop}></Route>
-                <Route path='/signin' exact component={Signin}></Route>
-                <Route path='/signup' exact component={Signup}></Route>
-                <PrivateRoute path='/user/dashboard' exact component={UserDashboard}></PrivateRoute>
-                <AdminRoute path='/admin/dashboard' exact component={AdminDashboard}></AdminRoute>
-                <AdminRoute path="/create/category" exact component={AddCategory}></AdminRoute>
-                <AdminRoute path="/create/product" exact component={AddProduct}></AdminRoute>
-            </Switch>
-        </BrowserRouter>
-    )
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Home}></Route>
+        <Route path="/shop" exact component={Shop}></Route>
+        <Route path="/signin" exact component={Signin}></Route>
+        <Route path="/signup" exact component={Signup}></Route>
+        <PrivateRoute
+          path="/user/dashboard"
+          exact
+          component={UserDashboard}
+        ></PrivateRoute>
+        <AdminRoute
+          path="/admin/dashboard"
+          exact
+          component={AdminDashboard}
+        ></AdminRoute>
+        <AdminRoute
+          path="/create/category"
+          exact
+          component={AddCategory}
+        ></AdminRoute>
+        <AdminRoute
+          path="/create/product"
+          exact
+          component={AddProduct}
+        ></AdminRoute>
+        <AdminRoute path="/admin/orders" exact component={Orders} />
+      </Switch>
+    </BrowserRouter>
+  );
 }
