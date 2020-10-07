f16eaa59b498725c671febb84071809c05b3937e
Olabisi <53159103+olabee87-stack@users.noreply.github.com>
2020-10-07 18:30:53 +0300

    Add route to get all orders made by a user

--- src/admin/apiAdmin.js
+++ src/admin/apiAdmin.js
@@ -1,52 +1,75 @@
-import { API } from '../config';
+import { API } from "../config";
 
-export const createCategory = (userId, token, category) => { //user(javascript object, then convert to json) comes from signup({name, email, password})
-    //console.log(user);
-return fetch(`${API}/category/create/${userId}`, {
-method: "POST",
-headers: {
-    Accept: 'application/json', //api responds with json data, so need to accept it
-    "Content-Type": "application/json",
-    Authorization: `Bearer ${token}`
-},
-//the actual data in the body, need to send it as a json string
-body: JSON.stringify(category)
-})
-//get either success or error
-.then(response => {
-    return response.json()
-})
-.catch(err => {
-    console.log(err);
-});
+export const createCategory = (userId, token, category) => {
+  //user(javascript object, then convert to json) comes from signup({name, email, password})
+  //console.log(user);
+  return (
+    fetch(`${API}/category/create/${userId}`, {
+      method: "POST",
+      headers: {
+        Accept: "application/json", //api responds with json data, so need to accept it
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      //the actual data in the body, need to send it as a json string
+      body: JSON.stringify(category),
+    })
+      //get either success or error
+      .then((response) => {
+        return response.json();
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+  );
 };
 
 //create product
-export const createProduct = (userId, token, product) => { //user(javascript object, then convert to json) comes from signup({name, email, password})
-    return fetch(`${API}/product/create/${userId}`, {
-method: "POST",
-headers: {
-    Accept: 'application/json', 
-    Authorization: `Bearer ${token}`
-},
-//the actual data in the body
-body: product
-})
-//get either success or error
-.then(response => {
-    return response.json()
-})
-.catch(err => {
-    console.log(err);
-});
+export const createProduct = (userId, token, product) => {
+  //user(javascript object, then convert to json) comes from signup({name, email, password})
+  return (
+    fetch(`${API}/product/create/${userId}`, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      //the actual data in the body
+      body: product,
+    })
+      //get either success or error
+      .then((response) => {
+        return response.json();
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+  );
 };
 
 export const getCategories = () => {
-    return fetch(`${API}/categories`, {
-        method: "GET"
+  return fetch(`${API}/categories`, {
+    method: "GET",
+  })
+    .then((response) => {
+      //if get response, return it in json
+      return response.json();
     })
-    .then(response => { //if get response, return it in json
-        return response.json();
-    })
-    .catch(err => console.log(err))
+    .catch((err) => console.log(err));
+};
+
+//@Return all orders - req to the backend
+export const listOrders = async (userId, token) => {
+  try {
+    const response = await fetch(`${API}/order/list/${userId}`, {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return response.json();
+  } catch (err) {
+    return console.log(err);
+  }
 };
