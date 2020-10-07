11afe8f27d2a1e93a9d0117795f0758cf343b43a
veronika333 <vera3@inbox.ru>
2020-10-07 10:11:27 +0300

    single card can be shown with conditional buttons

--- src/core/Card.js
+++ src/core/Card.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import ShowImage from './ShowImage';
+import moment from 'moment'; //time and date displayed user friendly (npm install moment)
 
 //default value of showViewProductButton is true. prom comes from Product component
 const Card = ({ product, showViewProductButton = true}) => {
@@ -15,17 +16,38 @@ const Card = ({ product, showViewProductButton = true}) => {
         )
     }
     
+const showAddToCartButton = () => {
     return (
-        
+<button className="btn btn-outline-warning mt-2 mb-2">
+        Add to Cart
+</button>
+    )
+};
+
+const showStock = (quantity) => {
+    return quantity > 0 ? 
+(<span className="badge-primary badge-pill">In stock: {quantity}</span>) :
+    (<span>Out of stock</span>)
+}
+
+    return ( 
 <div className="card">
-    <div className="card-header">{product.name}</div>
+    <div className="card-header name">{product.name}</div>
     <div className="card-body">
         <ShowImage item={product} url="product" />
-        <p>{product.description.substring(0, 100)}</p>
-    <p>{product.price}€</p>
-   
+        <p className="lead mt-2">{product.description.substring(0, 100)}</p>
+    <p className="black-10">{product.price}€</p>
+    <p className="black-9">
+        Category: {product.category && product.category.name}
+    </p>
+    <p className="black-8">
+        Added: {moment(product.createdAt).fromNow()} 
+    </p>
+{showStock(product.quantity)}   
+<br />    
 {showViewButton(showViewProductButton)}
-    <button className="btn btn-outline-warning mt-2 mb-2">Add to Card</button>
+{showAddToCartButton()}
+  
     </div>
 </div>
       
