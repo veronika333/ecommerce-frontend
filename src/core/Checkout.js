25368fc7ba23757698147fa2498e4035d5de1c92
Olabisi <53159103+olabee87-stack@users.noreply.github.com>
2020-10-12 12:17:46 +0300

    Add function to show dropIn interface when user is authenticated and want to checkout

--- src/core/Checkout.js
+++ src/core/Checkout.js
@@ -4,9 +4,39 @@ import Layout from "./Layout";
 import { getProducts, getBraintreeClientToken } from "./apiCore";
 import Card from "./Card";
 import { isAuthenticated } from "../auth";
+import DropIn from "braintree-web-drop-in-react";
 
 //get all the products in the cart and calculate total
 const Checkout = ({ products, setRun = (f) => f, run = undefined }) => {
+  //1. @State for token
+  const [data, setData] = useState({
+    success: false,
+    clientToken: null,
+    error: "",
+    instance: {}, //for react briantree drop
+    address: "",
+  });
+
+  //2. @get client token from the backend, pass userId and token to func
+  const userId = isAuthenticated() && isAuthenticated().user._id;
+  const token = isAuthenticated() && isAuthenticated().token;
+
+  //3. @gets userId and token - runs when there is a change in state
+  // - make req to the backend with getbraintreeClient defined method
+  const getToken = (userId, token) => {
+    getBraintreeClientToken(userId, token).then((data) => {
+      if (data.error) {
+        setData({ ...data, error: data.error });
+      } else {
+        setData({ ...data, clientToken: data.clientToken }); //gotten from the backend
+      }
+    });
+  };
+
+  useEffect(() => {
+    getToken(userId, token);
+  }, []);
+
   const getTotal = () => {
     //doc mozilla: array.prototype.reduce()
     return products.reduce((currentValue, nextValue) => {
@@ -14,15 +44,33 @@ const Checkout = ({ products, setRun = (f) => f, run = undefined }) => {
     }, 0);
   };
 
+  //@show DropIn interface if authenticated
   const showCheckout = () => {
     return isAuthenticated() ? (
-      <button className="btn btn-success">Checkout</button>
+      <button className="btn btn-success">{showDropIn()}</button>
     ) : (
       <Link to="/signin">
         <button className="btn btn-primary">Sign in to checkout</button>
       </Link>
     );
   };
+
+  //@show drop in UI - when the data contains a token - used inshowCheckOt above
+  const showDropIn = () => (
+    <div>
+      {data.clientToken !== null && products.length > 0 ? (
+        <div>
+          <DropIn
+            options={{
+              authorization: data.clientToken,
+            }}
+            onInstance={(instance) => (data.instance = instance)} //replaces the state of the instance
+          />
+          <button className="btn btn-success">Checkout</button>
+        </div>
+      ) : null}
+    </div>
+  );
 
   return (
     <div>
