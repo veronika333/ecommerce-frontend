c26286ab6c317ccc871055f60f2d70823f1a91bc
Olabisi <53159103+olabee87-stack@users.noreply.github.com>
2020-10-12 11:22:20 +0300

    import braintree client token

--- src/core/Checkout.js
+++ src/core/Checkout.js
@@ -1,34 +1,35 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import Layout from './Layout';
-import {getProducts} from './apiCore';
-import Card from './Card';
-import {isAuthenticated} from '../auth';
+import Layout from "./Layout";
+import { getProducts, getBraintreeClientToken } from "./apiCore";
+import Card from "./Card";
+import { isAuthenticated } from "../auth";
 
 //get all the products in the cart and calculate total
-const Checkout = ({products, setRun = f => f, run = undefined}) => {
-    const getTotal = () => { //doc mozilla: array.prototype.reduce()
-        return products.reduce((currentValue, nextValue) => {
-return currentValue + nextValue.count * nextValue.price;
-        }, 0) 
-    }
+const Checkout = ({ products, setRun = (f) => f, run = undefined }) => {
+  const getTotal = () => {
+    //doc mozilla: array.prototype.reduce()
+    return products.reduce((currentValue, nextValue) => {
+      return currentValue + nextValue.count * nextValue.price;
+    }, 0);
+  };
 
-const showCheckout = () => {
-  return isAuthenticated() ? (
-<button className="btn btn-success">Checkout</button>
-   ) : (
-<Link to="/signin">
-<button className="btn btn-primary">Sign in to checkout</button>
-</Link>   
-   ) 
-}
+  const showCheckout = () => {
+    return isAuthenticated() ? (
+      <button className="btn btn-success">Checkout</button>
+    ) : (
+      <Link to="/signin">
+        <button className="btn btn-primary">Sign in to checkout</button>
+      </Link>
+    );
+  };
 
- return (
- <div>
-   <h2>Total: ${getTotal()}</h2>
-   {showCheckout()}
-</div>
- )
-}
+  return (
+    <div>
+      <h2>Total: ${getTotal()}</h2>
+      {showCheckout()}
+    </div>
+  );
+};
 
 export default Checkout;
